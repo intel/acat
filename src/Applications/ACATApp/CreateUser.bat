0657d2ac5c6fe82054ea8c670e3ed52e355d8c82
saiprasadb01 <sai.prasad@intel.com>
2015-08-28 10:07:14 -0700

    Releasing a new version shortly

--- src/Applications/ACATApp/CreateUser.bat
+++
@@ -1,9 +0,0 @@
-if %1a==a goto default
-set ACATUSER=%1
-goto create
-:default
-set ACATUSER=Default
-:create
-if not exist Users mkdir Users
-if not exist Users\%ACATUSER% mkdir Users\%ACATUSER%
-xcopy /s /i /e /y install\Users\Default Users\%ACATUSER%

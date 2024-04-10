83ee448d14fab2466c65c9cb68daf4457107452e
Sai Prasad <sai.prasad@intel.com>
2024-04-10 16:21:49 -0700

    Updated version number to 3.01

--- src/Libraries/ACATCore/Properties/AssemblyInfo.cs
+++ src/Libraries/ACATCore/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Bdduild and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.00")]
-[assembly: AssemblyFileVersion("3.00")]
+[assembly: AssemblyVersion("3.01")]
+[assembly: AssemblyFileVersion("3.01")]
 

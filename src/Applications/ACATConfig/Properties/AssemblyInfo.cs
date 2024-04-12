4ed59550625cb3fe0ddccffb8ce5ddc46f727a9d
Sai Prasad <sai.prasad@intel.com>
2024-04-12 11:46:58 -0700

    Updated version to 3.10

--- src/Applications/ACATConfig/Properties/AssemblyInfo.cs
+++ src/Applications/ACATConfig/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.01")]
-[assembly: AssemblyFileVersion("3.01")]
+[assembly: AssemblyVersion("3.10")]
+[assembly: AssemblyFileVersion("3.10")]
 

55a7654285363a5dec6863c3cb80e89331ee277a
Sai Prasad <sai.prasad@intel.com>
2024-08-19 21:06:44 -0700

    Update Program.cs
    Add DialogSense to startup

--- src/Applications/ACATTalk/Program.cs
+++ src/Applications/ACATTalk/Program.cs
@@ -132,7 +132,8 @@ namespace ACAT.Applications.ACATTalk
                                 Context.StartupFlags.AgentManager |
                                 Context.StartupFlags.SpellChecker |
                                 Context.StartupFlags.WindowsActivityMonitor |
-                                Context.StartupFlags.Abbreviations))
+                                Context.StartupFlags.Abbreviations |
+                                Context.StartupFlags.DialogSense))
             {
                 splash.Close();
                 splash = null;

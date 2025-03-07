2d32ddc6dba6bd749f2f2802eecc74c1030ea240
Beale, Michael <michael.beale@intel.com>
2025-03-07 08:31:14 -0800

    Enable watchdog for window overlap detection in SplashScreen

--- src/Libraries/ACATCore/PanelManagement/UI/SplashScreen.cs
+++ src/Libraries/ACATCore/PanelManagement/UI/SplashScreen.cs
@@ -51,7 +51,7 @@ namespace ACAT.Lib.Core.PanelManagement
 
             TopMost = true;
 
-            //_watchDog = new WindowOverlapWatchdog(this);
+            _watchDog = new WindowOverlapWatchdog(this);
 
             Load += SplashScreen_Load;
             Shown += SplashScreen_Shown;

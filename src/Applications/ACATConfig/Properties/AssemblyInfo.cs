c06a0ef094e28b21a1ee925ad5757643d857cb81
Beale, Michael <michael.beale@intel.com>
2025-03-12 08:03:02 -0700

    Refactor ACATConfig and ACATTalk projects: remove unused app.config files, add NeutralResourcesLanguage attribute, and improve onboarding sequence with GUID constants

--- src/Applications/ACATConfig/Properties/AssemblyInfo.cs
+++ src/Applications/ACATConfig/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -35,4 +36,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.12")]
 [assembly: AssemblyFileVersion("3.12")]
-
+[assembly: NeutralResourcesLanguage("en")]

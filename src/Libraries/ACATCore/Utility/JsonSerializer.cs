ac705a64b1b027e515a5add4fe740b183791b7fa
Beale, Michael <michael.beale@intel.com>
2025-03-20 07:00:03 -0700

    Refactor JsonSerializer: remove commented NotImplementedException and improve code clarity

--- src/Libraries/ACATCore/Utility/JsonSerializer.cs
+++ src/Libraries/ACATCore/Utility/JsonSerializer.cs
@@ -1,16 +1,17 @@
-﻿namespace ACAT.Lib.Core.Utility
+﻿using System;
+
+namespace ACAT.Lib.Core.Utility
 {
     public static class JsonSerializer
     {
         public static string Serialize<TValue>(TValue message)
         {
-            //throw new NotImplementedException();
             return System.Text.Json.JsonSerializer.Serialize<TValue>(message);
+
         }
 
         public static TValue Deserialize<TValue>(string json)
         {
-            //throw new NotImplementedException();
             return System.Text.Json.JsonSerializer.Deserialize<TValue>(json);
         }
 

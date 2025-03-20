661fa246c55cc4597b4df5d2607c67c40e8699e5
Beale, Michael <michael.beale@intel.com>
2025-03-20 14:16:03 -0700

    Add System.Security reference to ACATConfig and ACATTalk projects; clean up unused references in Scanners project

--- src/Applications/ACATConfig/Program.cs
+++ src/Applications/ACATConfig/Program.cs
@@ -18,11 +18,11 @@ using ACAT.Lib.Core.Utility;
 using ACAT.Lib.Extension;
 using ACATExtension.CommandHandlers;
 using System;
-//using System.ComponentModel;
-//using System.IO;
-//using System.Reflection;
-//using System.Runtime.InteropServices;
-//using System.Security.Cryptography.Pkcs;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.Pkcs;
 using System.Windows.Forms;
 
 namespace ACATConfig

929783ac6cbf20016c6223836050a8079a17e287
Olmedo Garcia, Oscar <oscar.olmedo.garcia@intel.com>
2025-05-20 11:20:33 -0600

    Added languages on listboxlanguage select on usercontrollanguageselect code behit

--- src/Libraries/ACATExtension/Onboarding/UserControlLanguageSelect.cs
+++ src/Libraries/ACATExtension/Onboarding/UserControlLanguageSelect.cs
@@ -14,6 +14,9 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
+using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
 
 namespace ACAT.Lib.Extension.Onboarding
 {
@@ -22,13 +25,6 @@ namespace ACAT.Lib.Extension.Onboarding
     /// </summary>
     public partial class UserControlLanguageSelect : UserControl, IOnboardingUserControl
     {
-        public IActuator ActuatorSelected = null;
-        private const String bodyStyle = " background-color:#232433;";
-        private const String headStyle = "a:link{color: rgb(255, 170, 0);}";
-        private const String textStyle = "font-family:'Montserrat Medium'; font-size:24px; color:white";
-        private ActuatorConfig _actuatorConfig;
-        private readonly List<ActuatorSetting> _actuatorSettings = new List<ActuatorSetting>();
-        private readonly String _htmlTemplate = "<!DOCTYPE html><html><head><style>{0}</style></head><body style=\"{1}\"><p style=\"{2}\">{3}<font></body></html>";
         private readonly IOnboardingExtension _onboardingExtension;
         private readonly String _stepId;
 
@@ -56,42 +52,27 @@ namespace ACAT.Lib.Extension.Onboarding
             }
         }
 
-        public UserControl GetUserControl()
-        {
-            return this;
-        }
-
         public bool Initialize()
         {
-            bool retVal = true;
-
-            var actuators = Context.AppActuatorManager.Actuators;
-
-            _actuatorConfig = ActuatorConfig.Load();
-
-            foreach (var actuator in actuators)
-            {
-                var actuatorSetting = getAcutatorSetting(actuator.Descriptor.Id);
-                if (actuatorSetting != null)
-                {
-                    listBoxLanguages.Items.Add(actuatorSetting.Name);
-                    _actuatorSettings.Add(actuatorSetting);
-                }
-            }
-
-            listBoxLanguages.SelectedIndexChanged += ListBoxActuators_SelectedIndexChanged;
-
-            if (listBoxLanguages.Items.Count > 0)
-            {
-                listBoxLanguages.SelectedIndex = 0;
-            }
-
-            return retVal;
+            return true;
         }
 
         public void OnAdded()
         {
             listBoxLanguages.Focus();
+
+            var cultureInfos = ResourceUtils.EnumerateInstalledLanguages();
+            string defaultLanguage = (CultureInfo.DefaultThreadCurrentUICulture != null)
+                ? CultureInfo.DefaultThreadCurrentUICulture.DisplayName
+                : "[Not set]";
+
+            listBoxLanguages.Items.Add(defaultLanguage+"("+ CultureInfo.DefaultThreadCurrentUICulture.TwoLetterISOLanguageName+")");
+
+            foreach (var culture in cultureInfos)
+             {
+                 string text = culture.DisplayName + " (" + culture.TwoLetterISOLanguageName + ")";
+                listBoxLanguages.Items.Add(text);
+            }
         }
 
         public bool OnPreAdd()
@@ -117,136 +98,5 @@ namespace ACAT.Lib.Extension.Onboarding
         {
             return true;
         }
-
-        private IActuator getActuator(int index)
-        {
-            var actuatorSetting = _actuatorSettings[index];
-            foreach (var actuator in Context.AppActuatorManager.Actuators)
-            {
-                if (actuatorSetting.Id.Equals(actuator.Descriptor.Id))
-                {
-                    return actuator;
-                }
-            }
-
-            return null;
-        }
-
-        private ActuatorSetting getAcutatorSetting(Guid guid)
-        {
-            foreach (var actuatorSetting in _actuatorConfig.ActuatorSettings)
-            {
-                if (guid.Equals(actuatorSetting.Id))
-                {
-                    return actuatorSetting;
-                }
-            }
-
-            return null;
-        }
-
-        private void ListBoxActuators_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            ActuatorSelected = getActuator(listBoxLanguages.SelectedIndex);
-            var desc = _actuatorSettings[listBoxLanguages.SelectedIndex].Description;
-            if (!String.IsNullOrEmpty(desc))
-            {
-                String html = String.Format(_htmlTemplate, headStyle, bodyStyle, textStyle, HtmlUtils.DecodeHtml(desc));
-                Log.Debug(html);
-
-                html = html.Replace(CoreGlobals.MacroACATUserGuide, HtmlUtils.EncodeString(CoreGlobals.ACATUserGuideFileName));
-
-                ///webBrowserDesc.DocumentText = html;
-            }
-
-            var file = _actuatorSettings[listBoxLanguages.SelectedIndex].ImageFileName;
-
-            if (String.IsNullOrEmpty(file))
-            {
-                file = ActuatorSelected.OnboardingImageFileName;
-            }
-
-            Image image = null;
-            if (!String.IsNullOrEmpty(file))
-            {
-                image = ImageUtils.LoadImage(Path.Combine(FileUtils.GetImagePath(file)));
-            }
-
-           /// pictureBoxActuator.BackgroundImage = image;
-        }
-
-        private void WebBrowserDesc_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
-        {
-            //webBrowserDesc.Navigating -= WebBrowserDesc_Navigating;
-           // webBrowserDesc.Navigating += WebBrowserDesc_Navigating;
-        }
-
-        private void WebBrowserDesc_Navigating(object sender, WebBrowserNavigatingEventArgs e)
-        {
-            var str = e.Url.ToString();
-
-            Log.Debug("Url is [" + str + "]");
-
-            if (str.ToLower().Contains("blank"))
-            {
-                return;
-            }
-
-            e.Cancel = true;
-
-            String param1 = String.Empty;
-            String param2 = String.Empty;
-
-            if (str.Contains("about:"))
-            {
-                var index = str.IndexOf(':');
-
-                str = str.Substring(index + 1);
-
-                index = str.IndexOf('#');
-
-                if (index > 0)
-                {
-                    param1 = str.Substring(0, index);
-                    param2 = str.Substring(index + 1, str.Length - index -1);
-                }
-                else
-                {
-                    param1 = str;
-                }
-            }
-
-            List<String> list = new List<String>();
-            
-            if (param2.ToLower().EndsWith(".mp4"))
-            {
-                list.Add("Video");
-                list.Add(String.Empty);
-                list.Add(String.Empty);
-                list.Add((param2));
-                list.Add(String.Empty);
-            }
-            else if (param1.ToLower().EndsWith(".pdf"))
-            {
-                list.Add("PDF");
-                list.Add("true");
-                list.Add(Resources.PDFLoaderHtml);
-                list.Add(param1);
-                list.Add(param2);
-            }
-
-            try
-            {
-                HtmlUtils.LoadHtml(SmartPath.ApplicationPath, list.ToArray());
-            }
-            catch
-            {
-
-            }
-            finally
-            {
-
-            }
-        }
-    }
+   }
 }

179c7724af085be50ea154ec4c4458ebb1d652df
Beale, Michael <michael.beale@intel.com>
2025-06-10 10:26:35 -0700

    Enhance BCI device testing and UI initialization with improved error handling and layout adjustments Work in progress on updated BCI onboarding sequence.

--- src/Extensions/BCI/Actuators/gTec_SensorUI/SensorForm.cs
+++ src/Extensions/BCI/Actuators/gTec_SensorUI/SensorForm.cs
@@ -125,30 +125,36 @@ namespace ACAT.Extensions.BCI.Actuators.gTecSensorUI
             // user controls actually added to form
             _userControlTestBCIConnections = new UserControlTestBCIConnections();
             _userControlTestBCIConnections.buttonExit_userControlTestBCIConnections.Click += new EventHandler(this.buttonExit_Click);
-
+            _userControlTestBCIConnections.Dock = DockStyle.Fill;
             _userControlBCIErrorgTecBoard = new UserControlBCIErrorGTecBoard();
             _userControlBCIErrorgTecBoard.buttonExit_userControlBCIErrorgTecBoard.Click += new EventHandler(this.buttonExit_Click);
             _userControlBCIErrorgTecBoard.buttonRetry_userControlBCIErrorgTecBoard.Click += new EventHandler(this.buttonRetest_Click);
+            _userControlBCIErrorgTecBoard.Dock = DockStyle.Fill;
 
             _userControlErrorBluetoothDisconnected = new UserControlErrorBluetoothDisconnected();
             _userControlErrorBluetoothDisconnected.buttonExit_userControlErrorBluetoothDisconnected.Click += new EventHandler(this.buttonExit_Click);
             _userControlErrorBluetoothDisconnected.buttonNext_userControlErrorBluetoothDisconnected.Click += new EventHandler(this.buttonNext_Click);
+            _userControlErrorBluetoothDisconnected.Dock = DockStyle.Fill;
 
             _userControlBCISignalCheckStartRequired = new UserControlBCISignalCheckStartRequired();
             _userControlBCISignalCheckStartRequired.buttonExit_userControlBCISignalCheckStartRequired.Click += new EventHandler(this.buttonExit_Click);
             _userControlBCISignalCheckStartRequired.buttonNext_userControlBCISignalCheckStartRequired.Click += new EventHandler(this.buttonNext_Click);
+            _userControlBCISignalCheckStartRequired.Dock = DockStyle.Fill;
 
             _userControlBCISignalCheckStartPrompt = new UserControlBCISignalCheckStartPrompt();
             _userControlBCISignalCheckStartPrompt.buttonExit_userControlBCISignalCheckStartPrompt.Click += new EventHandler(this.buttonExit_Click);
             _userControlBCISignalCheckStartPrompt.buttonNext_userControlBCISignalCheckStartPrompt.Click += new EventHandler(this.buttonNext_Click);
+            _userControlBCISignalCheckStartPrompt.Dock = DockStyle.Fill;
 
             _userControlPromptBCIFIlterSettings = new UserControlBCIFilterSettings();
             _userControlPromptBCIFIlterSettings.buttonExit_userControlPromptBCIFIlterSettings.Click += new EventHandler(this.buttonExit_Click);
             _userControlPromptBCIFIlterSettings.buttonNext_userControlPromptBCIFIlterSettings.Click += new EventHandler(this.buttonNext_Click);
+            _userControlPromptBCIFIlterSettings.Dock = DockStyle.Fill;
 
             _userControlBCISignalCheck = new UserControlBCISignalCheck();
             _userControlBCISignalCheck.buttonExit_userControlBCISignalCheck.Click += new EventHandler(this.buttonExit_Click);
             _userControlBCISignalCheck.buttonNext_userControlBCISignalCheck.Click += new EventHandler(this.buttonNext_Click);
+            _userControlBCISignalCheck.Dock = DockStyle.Fill;
 
             _gTecBCI = gTecBCI;
 
@@ -160,6 +166,12 @@ namespace ACAT.Extensions.BCI.Actuators.gTecSensorUI
             {
                 // Set Exit button on each user control screen to [Developer Mode] which iterates through all available screens on button press
                 modifyUserControlsForDebugMode();
+
+                // tableLayoutPanelContainer.Controls.Clear();
+                // tableLayoutPanelContainer.Controls.Add(_userControlTestBCIConnections, 0, 0);
+                // tableLayoutPanelContainer.Refresh();
+
+                // _currentUserControlShown = _userControlTestBCIConnections;
             }
 
             Paint += (s, args) => { };

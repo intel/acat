8d5a6cd15b83d2b8e91caa1351ce819dc8427e34
Beale, Michael <michael.beale@intel.com>
2025-06-19 15:16:18 -0700

    Add Sentence and Word Prediction User Controls
    - Implemented SentencePredictionUserControl to display sentence predictions and phrases, with TTS functionality for selected phrases.
    - Created WordPredictionUserControl to show next word predictions.
    - Added corresponding .resx files for localization support.
    - Updated designer files for both user controls to define UI layout and components.

---
+++ src/Extensions/ACAT.Extensions.UI/Scanners/MouseScanner.cs
@@ -0,0 +1,324 @@
+﻿// Copyright 2013-2019; 2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+// MouseScanner.cs
+// This form is for the ACAT Mouse Scanner applications.  This scanner
+// enables the user to move the mouse around the display using grid scanning
+// technique.  The user can also click, double click, drag, right click etc.
+
+using ACAT.Lib.Core.Audit;
+using ACAT.Lib.Core.PanelManagement;
+using ACAT.Lib.Core.PanelManagement.CommandDispatcher;
+using ACAT.Lib.Core.Utility;
+using System;
+using System.Windows.Forms;
+using ACAT.Lib.Core.ActuatorManagement;
+using ACAT.Lib.Core.WidgetManagement;
+using ACAT.Lib.Extension.CommandHandlers;
+using ACAT.Lib.Core.AgentManagement;
+using System.Security.Permissions;
+using System.Collections.Generic;
+
+namespace ACAT.Lib.Extension
+{
+    [DescriptorAttribute("802B03F0-1294-4D06-A601-2CEBFBFA5D9C",
+                        "MouseScanner",
+                        "Enables mouse placement and mouse action on the display")]
+    public partial class MouseScanner : Form, IScannerPanel
+    {
+        private readonly Dispatcher _dispatcher;
+
+        private Widget _rootWidget;
+
+        private ScannerCommon _scannerCommon;
+
+        public MouseScanner(String panelClass, String title)
+        {
+            InitializeComponent();
+            _dispatcher = new Dispatcher(this);
+            Load += MouseScanner_Load;
+            FormClosing += MouseScanner_FormClosing;
+            Text = title;
+
+            // Add the mouse control buttons to the flow layout
+            // panel.  These are the buttons that will be used to control
+            // the mouse movement and actions.
+
+            var buttons = new List<ScannerButtonControl>
+                {
+                    new ScannerButtonControl { Name = "GoBack", AutoSize = true, AutoSizeMode = AutoSizeMode.GrowAndShrink, Dock = DockStyle.Fill },
+                    new ScannerButtonControl { Name = "ScanDown", AutoSize = true, AutoSizeMode = AutoSizeMode.GrowAndShrink, Dock = DockStyle.Fill },
+                    new ScannerButtonControl { Name = "ScanUp", AutoSize = true, AutoSizeMode = AutoSizeMode.GrowAndShrink, Dock = DockStyle.Fill },
+                    new ScannerButtonControl { Name = "AutoPosition", AutoSize = true, AutoSizeMode = AutoSizeMode.GrowAndShrink, Dock = DockStyle.Fill },
+                    new ScannerButtonControl { Name = "LeftClick", AutoSize = true, AutoSizeMode = AutoSizeMode.GrowAndShrink, Dock = DockStyle.Fill },
+                    new ScannerButtonControl { Name = "RightClick", AutoSize = true, AutoSizeMode = AutoSizeMode.GrowAndShrink, Dock = DockStyle.Fill },
+                    new ScannerButtonControl { Name = "LeftDoubleClick", AutoSize = true, AutoSizeMode = AutoSizeMode.GrowAndShrink, Dock = DockStyle.Fill },
+                    new ScannerButtonControl { Name = "LeftClickAndHold", AutoSize = true, AutoSizeMode = AutoSizeMode.GrowAndShrink, Dock = DockStyle.Fill }
+                };
+
+            MouseScannerButtons.Controls.AddRange(buttons.ToArray());
+        }
+
+        public virtual RunCommandDispatcher CommandDispatcher => _dispatcher;
+
+        public IDescriptor Descriptor => DescriptorAttribute.GetDescriptor(GetType());
+
+        public Form Form => this;
+
+        public String PanelClass
+        {
+            get;
+            protected set;
+        }
+
+        public IPanelCommon PanelCommon => _scannerCommon;
+
+        public ScannerCommon ScannerCommon => _scannerCommon;
+
+
+        public SyncLock SyncObj => _scannerCommon.SyncObj;
+
+        public ITextController TextController => _scannerCommon.TextController;
+
+        protected override CreateParams CreateParams => Windows.SetFormStyles(base.CreateParams);
+
+        protected override bool ShowWithoutActivation => true;
+
+        public virtual bool CheckCommandEnabled(CommandEnabledArg arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual bool Initialize(StartupArg startupArg)
+        {
+            PanelClass = startupArg.PanelClass;
+
+            _scannerCommon = new ScannerCommon(this)
+            {
+                PositionSizeController = { AutoPosition = false }
+            };
+
+            if (!_scannerCommon.Initialize(startupArg))
+            {
+                return false;
+            }
+
+            CommandDispatcher.Commands.Add(new CommandHandler("CmdScanVerticalDown"));
+            CommandDispatcher.Commands.Add(new CommandHandler("CmdScanVerticalUp"));
+            CommandDispatcher.Commands.Add(new CommandHandler("CmdLeftClick"));
+            CommandDispatcher.Commands.Add(new CommandHandler("CmdLeftDoubleClick"));
+            CommandDispatcher.Commands.Add(new CommandHandler("CmdLeftClickAndHold"));
+            CommandDispatcher.Commands.Add(new CommandHandler("CmdRightClick"));
+            CommandDispatcher.Commands.Add(new CommandHandler("CmdGoBack"));
+
+
+            _rootWidget = PanelCommon.RootWidget;
+            return true;
+        }
+
+        public void OnFocusChanged(WindowActivityMonitorInfo monitorInfo)
+        {
+            _scannerCommon.OnFocusChanged(monitorInfo);
+        }
+
+        public virtual void OnPause()
+        {
+            _scannerCommon.OnPause();
+        }
+
+        public virtual bool OnQueryPanelChange(PanelRequestEventArgs eventArg)
+        {
+            return true;
+        }
+
+        public virtual void OnResume()
+        {
+            _scannerCommon.OnResume();
+        }
+
+        public void OnWidgetActuated(WidgetActuatedEventArgs e, ref bool handled)
+        {
+            handled = false;
+        }
+
+        public void SetTargetControl(Form parent, Widget widget)
+        {
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            _scannerCommon.OnFormClosing(e);
+            base.OnFormClosing(e);
+        }
+        [EnvironmentPermission(SecurityAction.LinkDemand, Unrestricted = true)]
+        [EnvironmentPermission(SecurityAction.LinkDemand, Unrestricted = true)]
+        protected override void WndProc(ref Message m)
+        {
+            _scannerCommon?.HandleWndProc(m);
+            base.WndProc(ref m);
+        }
+
+        private void MouseScanner_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _scannerCommon.OnClosing();
+            _scannerCommon.Dispose();
+        }
+
+        private void MouseScanner_Load(object sender, EventArgs e)
+        {
+            _scannerCommon.OnLoad();
+
+            // Override all the stuff ScannerCommon did
+            _scannerCommon.PauseOverlapWatchdog();
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            ControlBox = true;
+
+            Context.AppActuatorManager.EvtSwitchHook += GridMouseEvtSwitchHook;
+            PanelCommon.AnimationManager.Start(_rootWidget);
+        }
+
+        private void GridMouseEvtSwitchHook(IActuatorSwitch switchObj, ref bool handled)
+        {
+            handled = false;
+
+            if (_gridMouseMover != null)
+            {
+                _gridMouseMover.Actuate();
+                handled = true;
+            }
+        }
+
+        //private readonly IActuator _calibrationSupportedActuator;
+        //private readonly bool _enableScanTimingConfigure = true;
+        private GridMouseMover _gridMouseMover;
+
+        private GridMouseMover createGridMouseMover()
+        {
+            var gridMouseMover = new GridMouseMover
+            {
+                GridRectangleSpeed = GetOptionalProperty(Common.AppPreferences, "MouseGridRectangleSpeed", 40),
+                GridRectangleCycles = GetOptionalProperty(Common.AppPreferences, "MouseGridRectangleCycles", 2),
+                GridLineSpeed = GetOptionalProperty(Common.AppPreferences, "MouseGridLineSpeed", 20),
+                GridLineCycles = GetOptionalProperty(Common.AppPreferences, "MouseGridRectangleCycles", 1),
+                GridLineThickness = GetOptionalProperty(Common.AppPreferences, "MouseGridLineThickness", 2),
+                EnableVerticalGridRectangle = true
+            };
+
+            return gridMouseMover;
+        }
+
+        private int GetOptionalProperty(object obj, string propertyName, int fallback = 0)
+        {
+            var prop = obj.GetType().GetProperty(propertyName);
+            return prop != null ? (int)prop.GetValue(obj) : fallback;
+        }
+
+        
+
+
+        ///// <summary>
+        ///// Event handler for mouse down.  Treat this as a switch
+        ///// activation.
+        ///// </summary>
+        ///// <param name="sender">event sender</param>
+        ///// <param name="mouseEventArgs">event args</param>
+        //private void MouseScannerScreen_EvtMouseDown(object sender, MouseEventArgs mouseEventArgs)
+        //{
+        //    if (_gridMouseMover != null)
+        //    {
+        //        _gridMouseMover.Actuate();
+        //    }
+        //}
+
+        /// <summary>
+        /// Starts moving the mouse in the grid mode, in the specified
+        /// direction
+        /// </summary>
+        /// <param name="direction">down or up</param>
+        private void startGridSweep(GridMouseMover.Direction direction)
+        {
+            OnPause();
+
+            _gridMouseMover = createGridMouseMover();
+
+            AuditLog.Audit(new AuditEventMouseMover(direction.ToString()));
+
+            _gridMouseMover.GridRectangleDirection = direction;
+
+            _gridMouseMover.Start();
+
+            _gridMouseMover = null;
+
+            OnResume();
+        }
+
+        /// <summary>
+        /// Handles all  the commands for the mouse scanner
+        /// </summary>
+        private class CommandHandler : RunCommandHandler
+        {
+            /// <summary>
+            /// Initializes an instance of the handler
+            /// </summary>
+            /// <param name="cmd">the command</param>
+            public CommandHandler(String cmd)
+                : base(cmd)
+            {
+            }
+
+            /// <summary>
+            /// Executes the command
+            /// </summary>
+            /// <param name="handled">set to true if handled</param>
+            /// <returns>true</returns>
+            public override bool Execute(ref bool handled)
+            {
+                handled = true;
+
+                var form = Dispatcher.Scanner.Form as MouseScanner;
+
+                switch (Command)
+                {
+                    case "CmdScanVerticalDown":
+                        form.startGridSweep(GridMouseMover.Direction.Down);
+                        break;
+
+                    case "CmdScanVerticalUp":
+                        form.startGridSweep(GridMouseMover.Direction.Up);
+                        break;
+
+
+                    //case "CmdLeftClick":
+                    //    MouseUtils.SimulateLeftMouseClick();
+                    //    break;
+
+                    //case "CmdLeftDoubleClick":
+                    //    MouseUtils.SimulateLeftMouseDoubleClick();
+                    //    break;
+
+                    //case "CmdLeftClickAndHold":
+                    //    MouseUtils.SimulateLeftMouseDrag();
+                    //    break;
+
+                    //case "CmdRightClick":
+                    //    MouseUtils.SimulateRightMouseClick();
+                    //    break;
+
+                    default:
+                        handled = false;
+                        break;
+                }
+
+                return true;
+            }
+        }
+
+        public class Dispatcher : DefaultCommandDispatcher
+        { 
+            public Dispatcher(IScannerPanel panel)
+                : base(panel)
+            {
+            }
+        }
+    }
+}
---
+++ src/Extensions/ACAT.Extensions.UI/Scanners/YesNoResponseScanner.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace ACAT.Extensions.Default.UI.Menus
+{
+    partial class YesNoResponseScanner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Row1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ItemText0 = new ACAT.Lib.Core.WidgetManagement.ScannerRoundedButtonControl();
+            this.ItemText1 = new ACAT.Lib.Core.WidgetManagement.ScannerRoundedButtonControl();
+            this.ItemText2 = new ACAT.Lib.Core.WidgetManagement.ScannerRoundedButtonControl();
+            this.ItemText3 = new ACAT.Lib.Core.WidgetManagement.ScannerRoundedButtonControl();
+            this.ItemText4 = new ACAT.Lib.Core.WidgetManagement.ScannerRoundedButtonControl();
+            this.PromptRow = new System.Windows.Forms.TableLayoutPanel();
+            this.Prompt = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.Row1.SuspendLayout();
+            this.PromptRow.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.Row1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.PromptRow, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1179, 393);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Row1
+            // 
+            this.Row1.ColumnCount = 5;
+            this.Row1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Row1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Row1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Row1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Row1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Row1.Controls.Add(this.ItemText0, 0, 0);
+            this.Row1.Controls.Add(this.ItemText1, 1, 0);
+            this.Row1.Controls.Add(this.ItemText2, 2, 0);
+            this.Row1.Controls.Add(this.ItemText3, 3, 0);
+            this.Row1.Controls.Add(this.ItemText4, 4, 0);
+            this.Row1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Row1.Location = new System.Drawing.Point(0, 235);
+            this.Row1.Margin = new System.Windows.Forms.Padding(0);
+            this.Row1.Name = "Row1";
+            this.Row1.RowCount = 1;
+            this.Row1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.Row1.Size = new System.Drawing.Size(1179, 158);
+            this.Row1.TabIndex = 3;
+            // 
+            // ItemText0
+            // 
+            this.ItemText0.BorderColor = System.Drawing.Color.DimGray;
+            this.ItemText0.BorderRadiusBottomLeft = 0;
+            this.ItemText0.BorderRadiusBottomRight = 0;
+            this.ItemText0.BorderRadiusTopLeft = 0;
+            this.ItemText0.BorderRadiusTopRight = 0;
+            this.ItemText0.BorderWidth = 0F;
+            this.ItemText0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ItemText0.FlatAppearance.BorderSize = 0;
+            this.ItemText0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ItemText0.Location = new System.Drawing.Point(0, 0);
+            this.ItemText0.Margin = new System.Windows.Forms.Padding(0);
+            this.ItemText0.Name = "ItemText0";
+            this.ItemText0.Size = new System.Drawing.Size(235, 158);
+            this.ItemText0.TabIndex = 4;
+            this.ItemText0.Text = ".";
+            this.ItemText0.UseMnemonic = false;
+            this.ItemText0.UseVisualStyleBackColor = true;
+            // 
+            // ItemText1
+            // 
+            this.ItemText1.BorderColor = System.Drawing.Color.DimGray;
+            this.ItemText1.BorderRadiusBottomLeft = 0;
+            this.ItemText1.BorderRadiusBottomRight = 0;
+            this.ItemText1.BorderRadiusTopLeft = 0;
+            this.ItemText1.BorderRadiusTopRight = 0;
+            this.ItemText1.BorderWidth = 0F;
+            this.ItemText1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ItemText1.FlatAppearance.BorderSize = 0;
+            this.ItemText1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ItemText1.Location = new System.Drawing.Point(235, 0);
+            this.ItemText1.Margin = new System.Windows.Forms.Padding(0);
+            this.ItemText1.Name = "ItemText1";
+            this.ItemText1.Size = new System.Drawing.Size(235, 158);
+            this.ItemText1.TabIndex = 0;
+            this.ItemText1.Text = ".";
+            this.ItemText1.UseMnemonic = false;
+            this.ItemText1.UseVisualStyleBackColor = true;
+            this.ItemText1.Click += new System.EventHandler(this.scannerRoundedButtonControl1_Click);
+            // 
+            // ItemText2
+            // 
+            this.ItemText2.BorderColor = System.Drawing.Color.DimGray;
+            this.ItemText2.BorderRadiusBottomLeft = 0;
+            this.ItemText2.BorderRadiusBottomRight = 0;
+            this.ItemText2.BorderRadiusTopLeft = 0;
+            this.ItemText2.BorderRadiusTopRight = 0;
+            this.ItemText2.BorderWidth = 0F;
+            this.ItemText2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ItemText2.FlatAppearance.BorderSize = 0;
+            this.ItemText2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ItemText2.Location = new System.Drawing.Point(470, 0);
+            this.ItemText2.Margin = new System.Windows.Forms.Padding(0);
+            this.ItemText2.Name = "ItemText2";
+            this.ItemText2.Size = new System.Drawing.Size(235, 158);
+            this.ItemText2.TabIndex = 1;
+            this.ItemText2.Text = ".";
+            this.ItemText2.UseMnemonic = false;
+            this.ItemText2.UseVisualStyleBackColor = true;
+            // 
+            // ItemText3
+            // 
+            this.ItemText3.BorderColor = System.Drawing.Color.DimGray;
+            this.ItemText3.BorderRadiusBottomLeft = 0;
+            this.ItemText3.BorderRadiusBottomRight = 0;
+            this.ItemText3.BorderRadiusTopLeft = 0;
+            this.ItemText3.BorderRadiusTopRight = 0;
+            this.ItemText3.BorderWidth = 0F;
+            this.ItemText3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ItemText3.FlatAppearance.BorderSize = 0;
+            this.ItemText3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ItemText3.Location = new System.Drawing.Point(705, 0);
+            this.ItemText3.Margin = new System.Windows.Forms.Padding(0);
+            this.ItemText3.Name = "ItemText3";
+            this.ItemText3.Size = new System.Drawing.Size(235, 158);
+            this.ItemText3.TabIndex = 2;
+            this.ItemText3.Text = ".";
+            this.ItemText3.UseMnemonic = false;
+            this.ItemText3.UseVisualStyleBackColor = true;
+            // 
+            // ItemText4
+            // 
+            this.ItemText4.BorderColor = System.Drawing.Color.DimGray;
+            this.ItemText4.BorderRadiusBottomLeft = 0;
+            this.ItemText4.BorderRadiusBottomRight = 0;
+            this.ItemText4.BorderRadiusTopLeft = 0;
+            this.ItemText4.BorderRadiusTopRight = 0;
+            this.ItemText4.BorderWidth = 0F;
+            this.ItemText4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ItemText4.FlatAppearance.BorderSize = 0;
+            this.ItemText4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ItemText4.Location = new System.Drawing.Point(940, 0);
+            this.ItemText4.Margin = new System.Windows.Forms.Padding(0);
+            this.ItemText4.Name = "ItemText4";
+            this.ItemText4.Size = new System.Drawing.Size(239, 158);
+            this.ItemText4.TabIndex = 3;
+            this.ItemText4.Text = ".";
+            this.ItemText4.UseMnemonic = false;
+            this.ItemText4.UseVisualStyleBackColor = true;
+            // 
+            // PromptRow
+            // 
+            this.PromptRow.ColumnCount = 1;
+            this.PromptRow.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.PromptRow.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.PromptRow.Controls.Add(this.Prompt, 0, 0);
+            this.PromptRow.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PromptRow.Location = new System.Drawing.Point(0, 0);
+            this.PromptRow.Margin = new System.Windows.Forms.Padding(0);
+            this.PromptRow.Name = "PromptRow";
+            this.PromptRow.RowCount = 2;
+            this.PromptRow.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.PromptRow.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.PromptRow.Size = new System.Drawing.Size(1179, 235);
+            this.PromptRow.TabIndex = 1;
+            // 
+            // Prompt
+            // 
+            this.Prompt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Prompt.Location = new System.Drawing.Point(3, 0);
+            this.Prompt.Name = "Prompt";
+            this.PromptRow.SetRowSpan(this.Prompt, 2);
+            this.Prompt.Size = new System.Drawing.Size(1173, 235);
+            this.Prompt.TabIndex = 0;
+            this.Prompt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // YesNoResponseScanner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(36)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(1179, 393);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "YesNoResponseScanner";
+            this.Text = "ACAT";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.Row1.ResumeLayout(false);
+            this.PromptRow.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel Row1;
+        private System.Windows.Forms.TableLayoutPanel PromptRow;
+        private System.Windows.Forms.Label Prompt;
+        private Lib.Core.WidgetManagement.ScannerRoundedButtonControl ItemText1;
+        private Lib.Core.WidgetManagement.ScannerRoundedButtonControl ItemText2;
+        private Lib.Core.WidgetManagement.ScannerRoundedButtonControl ItemText3;
+        private Lib.Core.WidgetManagement.ScannerRoundedButtonControl ItemText4;
+        private Lib.Core.WidgetManagement.ScannerRoundedButtonControl ItemText0;
+    }
+}

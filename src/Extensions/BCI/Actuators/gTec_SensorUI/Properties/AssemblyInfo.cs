9f8d034440e73e5e41ca90b061504284677f686f
Savur, Celal <celal.savur@intel.com>
2025-07-17 14:51:51 -0700

    small changes..

--- src/Extensions/BCI/Actuators/gTec_SensorUI/Properties/AssemblyInfo.cs
+++ src/Extensions/BCI/Actuators/gTec_SensorUI/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("gTecSensorUI")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,4 +19,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ec1e8088-33a6-4593-832a-bf4722806ab2")]
+[assembly: Guid("04264F5E-CF7D-4289-A45A-D81BFB6A6441")]

0b71b11b99f3b63410e956db525e7be5f81c4518
Michael <michael.beale@intel.com>
2025-07-23 12:53:18 -0700

    minor changes to finish re-enabling BCI code.

--- src/Applications/ACATTalk/Program.cs
+++ src/Applications/ACATTalk/Program.cs
@@ -103,9 +103,6 @@ namespace ACAT.Applications.ACATTalk
 
             AuditLog.Audit(new AuditEvent("Application", "start"));
 
-            AppCommon.addBCIActuatorSetting();
-            AppCommon.addPanelClassConfigMapForBCI();
-
             CommandDescriptors.Init();
 
             Common.AppPreferences.PreferredPanelConfigNames = String.Empty;

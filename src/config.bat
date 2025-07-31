b174e535e4adc346830152c697fe7ba54718113e
Beale, Michael <michael.beale@intel.com>
2025-07-31 09:13:51 -0700

    Refactor project structure: remove obsolete batch files and update output paths in build configurations

--- src/config.bat
+++
@@ -1,3 +0,0 @@
-set ACAT_APPDIR=Applications\ACATApp
-set ACAT_EXTENSIONDIR=Extensions\Default
-set ACAT_USERINSTALLDIR=Install\Users\DefaultUser
--- src/deployRedist.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set SOURCEFILE=%1
-set TARGETDIR=%2\Redistributable
-
-if not exist %TARGETDIR% mkdir %TARGETDIR%
-
-if exist %SOURCEFILE% copy /y %SOURCEFILE% %TARGETDIR%
-
